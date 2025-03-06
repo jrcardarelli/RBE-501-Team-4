bd8dcdb3bf5ac8c422aede26163837f95f90057a
Joseph Cardarelli <100492431+jrcardarelli@users.noreply.github.com>
2025-03-05 20:41:50 -0500

    Delete build directory

--- build/arm_robot_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tamar/RBE501_ws/src/arm_robot_description
-# Build directory: /home/tamar/RBE501_ws/build/arm_robot_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/final_project/share/final_project/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/tamar/RBE501_ws/build/final_project"
